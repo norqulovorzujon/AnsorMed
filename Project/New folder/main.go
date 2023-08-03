b7ab6108aebfdb22716614031e7982bee314ec12
norqulovorzujon <norqulovorzujon00@gmail.com>
2023-08-03 21:15:51 +0500

    first commit

---
+++ Project/New folder/main.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func main(){
+	fmt.Println("hello world")
+	x:=20
+	y:=20
+	men:=x+y
+	fmt.Println(men)
+	i:=12
+	if i==10{
+		fmt.Println("javob 10")
+	// }else if i<100{
+	// 	fmt.Println("100 katta")
+	}else if i>100 {
+		fmt.Println("100 dan katta")
+	}else{
+		fmt.Println("javob yuq")
+	}
+}
